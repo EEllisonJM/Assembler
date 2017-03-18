6594a98f05d6ac273cf477bb9d68e32397dc783b
EEllisonJM <ellison_jm@hotmail.com>
2017-03-17 18:44:15 -0600

    Update MayorMenor.asm
    Programa en el que: se llena un arreglo de 5 elementos,

--- PROGRAMAS_A/MayorMenor.asm
+++ PROGRAMAS_A/MayorMenor.asm
@@ -1,23 +1,25 @@
-;INGRESAR 5 NUMEROS A UN ARREGLO
+;INGRESAR 5 NUMEROS A UN ARREGLO (DENTRO DE UN CICLO)
 ;MOSTRAR MAYOR
 ;MOSTRAR MENOR
+;MOSTRAR OPCIONES 1 y 2
+;1 -> Continuar
+;2 -> Salir
 
 .286
-sPila SEGMENT STACK
+sPila SEGMENT STACK ; Inicio segmento de Pila
     DB 32 DUP('STACK---')
-sPila ENDs
+sPila ENDs ; Fin segmento de Pila
 ;------------------------------------------------------------------------------
-sDatos SEGMENT
+sDatos SEGMENT ; Inicio segmento de Datos
 	opciones DB 0DH, 0AH,  '1 -> Continuar', 0DH, 0AH, '2 -> Salir ',0DH, 0AH,'$'
     msj   	 DB 0DH, 0AH,  'Ingrese dato : $'
     msjMayor DB 0DH, 0AH,  'El mayor es :  $'
 	msjMenor DB 0DH, 0AH,  'El menor es :  $'
     salto 	 DB 0DH, 0AH,  '$'
-    arre 	 DB 5 DUP('?'),'$'	; Arreglo de 5 posiciones [0,1,2,3,4]
-	
-sDatos ENDs
+    arre 	 DB 5 DUP('?'),'$'	; Arreglo de 5 posiciones [0,1,2,3,4]	
+sDatos ENDs ; Fin segmento de Datos
 ;------------------------------------------------------------------------------
-sCodigo SEGMENT 'CODE'
+sCodigo SEGMENT 'CODE' ; Inicio segmento de Codigo
     ASSUME SS:sPila, DS:sDatos, CS:sCodigo
     main PROC FAR ; Definicion del procedimiento FAR
         PUSH DS
@@ -31,7 +33,7 @@ Ciclo_Continuar_Salir:
 		MOV AH, 09H ; Funcion 09H -> Visualización de una cadena de caracteres
 		INT 21H	; Interrupcion del DOS
 		
-;----------------WRITE( _ )------------------------------------------------------
+;----------------READ( _ )------------------------------------------------------
 		MOV SI,0 ; Posicion inicial del arreglo		
 leer_ciclo:
 		MOV AH, 01H ; Función 01H -> Entrada de Carácter
@@ -83,10 +85,12 @@ menor_encontrado :
 		MOV AH,02H ; Función 02H -> Salida de Carácter 
 		INT 21H ; Interrupcion del DOS
 Menu:
+;----------------PRINT(1 -> Continuar')------------------------------------------------------
+;----------------PRINT(2 -> Salir )------------------------------------------------------
 		LEA DX,opciones
 		MOV AH,09h
 		INT 21H
-	
+;----------------READ( _ )------------------------------------------------------	
 		MOV AH, 01H
 		INT 21H
 		SUB AL, 30H ; Obtener numero en decimal [ASCII]
@@ -100,7 +104,8 @@ Menu:
 		
 Salirse:
 		RET ; Retorno al procedimiento mas cercano [Al OS:=Sistema Operativo]
-	main ENDp ; Final del procedimiento FAR
-sCodigo ENDs
+		
+	main ENDp ; Fin procedimiento FAR	
+sCodigo ENDs ; Fin segmento de Codigo
 END main
 
